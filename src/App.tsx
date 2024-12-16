d8fe015e0a4f42a32e78d1a5bc4122d1b069a501
Nyx <alternyxx@gmail.com>
2024-12-16 15:28:34 +0630

    a bunch of changes, probably deploying this stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from "react"
 import Canvas from "./components/Canvas"
-import BasicAbout from "./components/BasicAbout"
-import "./components/BasicAbout.css"
+import BasicAbout from "./components/BasicAbout/BasicAbout"
+import About from "./components/About/About"
+
+interface App {
+  device: GPUDevice,
+  warning: boolean
+}
 
 export default function App(props: any) {
   const [stage, setStage] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => setStage(1), 7000);
+    setTimeout(() => setStage(1), 9000);
   }, []);
 
   return (
@@ -15,6 +20,10 @@ export default function App(props: any) {
       <div className="App">
         <Canvas stage={stage} device={props.device} />
         <BasicAbout />
+        {stage === 1 &&
+          <>
+            <About />
+          </> }
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-let warning;
+let warning = false;
 let device;
 
 // Checks
@@ -12,10 +12,10 @@ if (navigator.gpu) {
     if (adapter) {
         device = await adapter.requestDevice();
     } else {
-        warning = "No appropriate GPUAdapter found";
+        warning = true;
     }
 } else {
-    warning = "WebGPU not supported on this browser.";    
+    warning = true;    
 }
 
 createRoot(document.getElementById('root')!).render(
