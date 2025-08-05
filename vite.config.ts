001d6855da5a9ac78426dae863edf405fbf27f2a
alternyxx <alternyxx@gmail.com>
2025-08-06 02:43:44 +0630

    Cloudflare worker config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // trying to get v4 to work with the .css files frustrated me so fucking much
 
 // https://vite.dev/config/
@@ -8,6 +10,7 @@ export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
+        cloudflare(),
     ],
     esbuild: {
         supported: {
